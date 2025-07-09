34a1a7945cbec294482721371f53303ccd9b3471
DasNoCode <debayan.hokkaido@gmail.com>
2025-07-09 12:02:31 +0530

    Update MessageHandler.py

--- src/Handler/MessageHandler.py
+++ src/Handler/MessageHandler.py
@@ -2,7 +2,6 @@ import importlib.util
 import os
 import re
 import random
-from datetime import datetime
 from Structures.Client import SuperClient
 from Structures.Message import Message
 
@@ -21,122 +20,78 @@ class MessageHandler:
             return
 
         isCommand = messageText.startswith(self.__client.prifix)
-
-        # === AFK Handling ===
-        mentionedUser = M.mentioned[0] if M.mentioned else None
-        mentionedUserId = getattr(mentionedUser, "user_id", None)
-        mentionedAFK = (
-            self.__client.db.User.get_user(mentionedUserId).get("afk", {"is_afk": False})
-            if mentionedUserId else {"is_afk": False}
-        )
-
-        repliedUser = getattr(M.reply_to_message, "replied_user", None)
-        repliedUserId = repliedUser.user_id if repliedUser else None
-        repliedAFK = self.__client.db.User.get_user(repliedUserId).get("afk", {"is_afk": False})
-
-        userData = self.__client.db.User.get_user(M.sender.user_id)
-
-        if M.message.split()[0] != "/afk":
-            if userData["afk"]["is_afk"]:
-                currentTime = datetime.now().time().strftime("%H:%M:%S")
-                self.__client.db.User.set_afk(M.sender.user_id, False, None, currentTime)
-                await self.__client.send_message(
-                    M.chat_id,
-                    f"__@{M.sender.user_name} nice to see you again!__"
-                )
-
-        if mentionedAFK["is_afk"] and repliedAFK["is_afk"]:
-            return await self.__client.send_message(
-                M.chat_id,
-                f"__@{M.sender.user_name} @{mentionedUser.user_name} is currently offline.\n"
-                f"**Reason** : {mentionedAFK.get('afk_reason', 'None')}__"
-            )
-        elif mentionedAFK["is_afk"]:
-            await self.__client.send_message(
-                M.chat_id,
-                f"__@{M.sender.user_name} @{mentionedUser.user_name} is currently offline.\n"
-                f"**Reason** : {mentionedAFK.get('afk_reason', 'None')}__"
-            )
-        elif repliedAFK["is_afk"]:
-            await self.__client.send_message(
-                M.chat_id,
-                f"__@{M.sender.user_name} @{repliedUser.user_name} is currently offline.\n"
-                f"**Reason** : {repliedAFK.get('afk_reason', 'None')}__"
-            )
+        commandName = commandContext[0]
+        commandObj = self.commandMap.get(commandName)
 
         # === Message Logging if Not Command ===
         if not isCommand:
             self.__client.log.info(
                 f"[MSG]: From {M.chat_type} by {M.sender.user_name} "
-                f"({'ADMIN' if M.isAdmin else 'NOT ADMIN'})"
+                f"({M.user_status})"
             )
             return
-        # === Command Parsing and Execution ===
-        if messageText == self.__client.prifix:
-            return await self.__client.send_message(
-                M.chat_id,
-                f"__Enter a command following {self.__client.prifix}__"
-            )
-
-        commandName = commandContext[0]
-        commandObj = self.commandMap.get(commandName)
 
         if not commandObj:
             return await self.__client.send_message(
                 M.chat_id,
-                "__Command does not available!!__"
+                "Command does not available!!"
+            )
+
+        if commandObj.config.ChatOnly and M.chat_type not in ["GROUP", "SUPERGROUP", "CHANNEL"]:
+            return await self.__client.send_message(M.chat_id, f"@{M.sender.user_name} this command can't be used in PRIVATE chat!")
+
+        if messageText == self.__client.prifix:
+            return await self.__client.send_message(
+                M.chat_id,
+                f"Enter a command following {self.__client.prifix}"
             )
 
         if commandObj.config.xp:
             try:
-               await self.__client.xp_lvl("USER",M)
+                await self.__client.xp_lvl("USER", M)
             except Exception as e:
-               self.__client.log.info(e)
-               pass
+                self.__client.log.info(e)
+                pass
 
         if commandObj.config.OwnerOnly and M.sender.user_id != self.__client.owner_id:
             return await self.__client.send_message(
                 M.chat_id,
-                "__This command can only be used by the **BOT owner**!!__"
+                "This command can only be used by the **BOT owner**!!"
             )
 
-        if commandObj.config.AdminOnly and M.isAdmin:
+        if commandObj.config.AdminOnly and M.isAdmin is False:
             return await self.__client.send_message(
                 M.chat_id,
-                "__This command can only be used by an admin!!__"
+                "This command can only be used by an admin!!"
             )
 
         self.__client.log.info(
             f"[CMD]: {self.__client.prifix}{commandName} from {M.chat_type} "
             f"by {M.sender.user_name} "
-            f"({'ADMIN' if M.isAdmin else 'NOT ADMIN'})"
+            f"{M.user_status}"
         )
 
         await commandObj.exec(M, commandContext)
-        
-        try: 
-            if M.chat_type == "SUPERGROUP" :
-                chat_data = self.__client.db.Chat.get_chat_data(M.chat_id)
-                msg_count = chat_data.get("stats").get("msg_count", 0) + 1
-                self.__client.db.Chat.update_chat_datas(M.chat_id, {"stats": {"msg_count": msg_count}})
-                if M.sender.user_id not in chat_data.get("stats").get("active_users") :
-                    active_users = chat_data.get("stats").get("active_users")
-                    active_users.append(M.sender.user_id)
-                    self.__client.db.Chat.update_chat_datas(M.chat_id, {"stats": {"active_users": active_users}})
-    
-                milestones = [500, 1000, 5000, 10000, 20000, 50000, 100000]
-                if msg_count in milestones:
-                    await self.__client.send_message(
-                        M.chat_id,
-                        f"🎉 Congratulations! This group has reached **{msg_count:,}** messages! Keep chatting!"
-                    )
-                await self.__client.xp_lvl("CHAT", M, random.randint(1, 2))
-        except Exception as e:
-            self.__client.log.info(e)
-            pass
 
-            
-
+        if M.chat_type == "SUPERGROUP":
+            chat_data = self.__client.db.Chat.get_chat_data(M.chat_id)
+            messages_count = chat_data.get("stats", {}).get("messages_count", 0) + 1
+            active_users = chat_data.get("stats", {}).get("active_users", [])
+            if M.sender.user_id not in active_users:
+                active_users.append(M.sender.user_id)
+            self.__client.db.Chat.update_chat_datas(M.chat_id, {
+                "stats": {
+                    "messages_count": messages_count,
+                    "active_users": active_users
+                }
+            })
+            milestones = [500, 1000, 5000, 10000, 20000, 50000, 100000]
+            if messages_count in milestones:
+                await self.__client.send_message(
+                    M.chat_id,
+                    f"🎉 Congratulations! This group has reached **{messages_count:,}** messages! Keep chatting!"
+                )
+                await self.__client.xp_lvl("CHAT", M, random.randint(1, 5))
 
     def loadCommands(self, folderPath):
         for fileName in os.listdir(folderPath):
